5dc071aeb36d9e1ca5f1d03f3a7dc4af44247f32
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2025-03-17 21:59:57 -0300

    commit

---
+++ todolist/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/data/local/task_dao.dart';
+import 'package:todolist/repository/task_repository.dart';
+import 'package:todolist/view/screens/home_screen.dart';
+import 'package:todolist/viewmodel/task_viewmodel.dart';
+
+void main() {
+  final taskDao = TaskDao();
+  final repository = TaskRepository(taskDao: taskDao);
+  runApp(
+    ChangeNotifierProvider(create: (_) => TaskViewModel(repository: repository),
+    child: MyApp (),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(title: 'todo list',
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen());
+  }
+}
